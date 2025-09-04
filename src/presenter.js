cb7d1ab19ecc1a336344caf018db15e953f6a8d4
Lackio-EL-14 <edsonlamas14@gmail.com>
2025-09-04 09:47:16 -0400

    feat: Ahora muestra el estado

--- src/presenter.js
+++ src/presenter.js
@@ -3,9 +3,12 @@ import totalizador from "./totalizador.js";
 document.getElementById('calcular').addEventListener('click', () => {
   const cantidad = parseInt(document.getElementById('cantidad').value, 10);
   const precio = parseFloat(document.getElementById('precio').value);
+  let estado = document.getElementById('estado').value.trim().toUpperCase();
 
-  const resultado = totalizador(cantidad, precio);
+  estado = estado.slice(0, 2);
+
+  const resultado = totalizador(cantidad, precio, estado);
 
   document.getElementById('resultado').innerText =
-    `Cantidad: ${resultado.cantidad}, Precio: ${resultado.precio}`;
+    `Cantidad: ${resultado.cantidad}, Precio: ${resultado.precio}, Estado: ${resultado.estado}`;
 });
